53ee75ff975da0429a75272794464ef0eeb751cf
Fco. Javier Fdez. Collados <xabyer.b@gmail.com>
2024-10-14 19:09:56 +0200

    WinForms Course Pavol Almasi Project 1, Stop Watch. Creating GUI, buttons added.

---
+++ WinFormsProjects/Pr01StopWatch/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Pr01StopWatch
+{
+    partial class frmMainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnStart = new Button();
+            btnStop = new Button();
+            btnExit = new Button();
+            SuspendLayout();
+            // 
+            // btnStart
+            // 
+            btnStart.Location = new Point(59, 48);
+            btnStart.Name = "btnStart";
+            btnStart.Size = new Size(75, 23);
+            btnStart.TabIndex = 0;
+            btnStart.Text = "&Start";
+            btnStart.UseVisualStyleBackColor = true;
+            // 
+            // btnStop
+            // 
+            btnStop.Location = new Point(59, 105);
+            btnStop.Name = "btnStop";
+            btnStop.Size = new Size(75, 23);
+            btnStop.TabIndex = 1;
+            btnStop.Text = "S&top";
+            btnStop.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            btnExit.Location = new Point(59, 163);
+            btnExit.Name = "btnExit";
+            btnExit.Size = new Size(75, 23);
+            btnExit.TabIndex = 2;
+            btnExit.Text = "&Exit";
+            btnExit.UseVisualStyleBackColor = true;
+            // 
+            // frmMainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnExit);
+            Controls.Add(btnStop);
+            Controls.Add(btnStart);
+            Name = "frmMainForm";
+            Text = "Stop Watch";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnStart;
+        private Button btnStop;
+        private Button btnExit;
+    }
+}
---
+++ WinFormsProjects/Pr01StopWatch/Form1.cs
@@ -0,0 +1,10 @@
+namespace Pr01StopWatch
+{
+    public partial class frmMainForm : Form
+    {
+        public frmMainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
